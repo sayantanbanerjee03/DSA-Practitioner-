d7c838de9876169dda070b6d750fccb454a9e005
Sayantan Banerjee <117519528+sayantanbanerjee03@users.noreply.github.com>
2024-03-12 19:54:50 +0530

    Add files via upload

---
+++ Input&Output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+ 
+    int n,s,g;
+    cin >> s >> n >> g;
+    cout << s+n+g << endl;
+ return 0;
+}
